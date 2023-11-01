364e8532afe9ff71270d9a6d9f717aa512409b51
IHEEB22 <chihebeddine.hmida@ensi-uma.tn>
2023-11-01 05:22:37 +0100

    Update availabe features
    This commit includes the following enhancements :
    # Area management: we can get ride of the new area button and make it a text form
    -->Done
    # When Scannig an area we enter the Area name > create a Table with the name of the area
    -->Done
    # expand the data base to accepte more then one Table (one table per area)
    -->Done
    # Fuse the where am i ? button with the Compute position button
    -->Done
    # Separate the scan area frame from the rest of the parameters
    -->Done
    # Modularity and integrity (function and modules responsability mangments)
    -->In progress
    # Log file configuration to track data base infos
    -->Frozen: it's not a priority
    # The pop out window showing the postion: improve the design
    --> Frozen: nothing to add, we can show the result in a text form within the main app frame
    
    ### Basically, and in addition to the enhacement mentioned i have reshaped the APP's GUI as well as
    setting access permission for administrators in order to manage the database.

--- Finger_Print_Technique/LAB - ENSI_Couloir_salle TP2_TP1/APP.py
+++ Finger_Print_Technique/LAB - ENSI_Couloir_salle TP2_TP1/APP.py
@@ -1,43 +1,127 @@
-#Next enhancment :
-# The pop out window showing the postion: how to improve the design
-# Area mangment: we can get ride of the new area button and make it a text form 
-# expand the data base to accept more than one Table (one table per area)
-# Fuse the where am i ? button with the Compute position button
-# Separate the scan area frame from the rest of the parameters  
-# Modularity and integrity (function and modules responsability mangments)
-# Log file configuration to track data base infos   
-
 import tkinter as tk
 from tkinter import messagebox
 import Modules as md
 
+#To access as an administrator
+correct_login = "chiheb"
+correct_password = "chichi123"
+
+##User guide##
+print("\n\n/************User Guide***********/\n\n")
+
+print("This finger-printing based localisation App Provide the following features:\n") 
+print("1_ Define an indoor Enviorement:\n",
+    " - Use the name of the area as a label for the Enviorement.\n",
+    " - Record multiple reference points within this area (location,fingerprint).\n",
+    " - Update reference Points within a spcific area.\n",
+    " - By initilaizing the area you can record from scratch new reference points associated to this area \n")
+print("2_ Get your Current Psition (after defining the enviorement):\n",
+      " - Enter the area name you are at using the <Am at> text field\n",
+      " - Identify your exact current position using the <Postion Tag> text field\n",
+      " - Press the 'where am I' button to get the result\n")
 
 
 #Data base intialization
 conn, cur = md.initialize_database()
 
-#RSSI Vector intitialization 
-Rssi_vector = []
+def admin_config():    
+    #Log available Enviorements
+    print("Available Enviorements:\n") 
+    try:
+        # Get a list of availabe tables in the database (each table represent an area = enviorement)
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        table_names = cur.fetchall()
+        table_list =[]
+        # Iterate through the table names and print each table name 
+        for table in table_names:
+            print(table[0],"\n")
+            table_list.append(table[0])
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
+    # Manage database
+    while True:
+        area_name = input("Choose an area name to display the recorded reference points within (or press Enter to exit):\n")
+        if area_name == '':
+            break  # Exit the loop if Enter is pressed
 
+        if area_name in table_list:
+            md.print_table_content(cur, area_name)
+        else:
+            print(f"The area '{area_name}' is not available in the database.\n")
+
+    while True:
+        table_name = input("Please provide the area name you want to delete (or press Enter to exit):\n")
+        if table_name == '':
+            break  # Exit the loop if Enter is pressed
+
+        if table_name in table_list:
+            respond = input(f"Are you sure you want to delete area '{table_name}' (Y/N):\n").strip().lower()
+            if respond == 'y':
+                md.delete_table(conn, cur, table_name)
+            else:
+                print("Deletion canceled.\n")
+        else:
+            print(f"The area '{table_name}' is not available in the database.\n")
+    print("You can still use the App GUI") 
 
 def scan():
+    area_name = entry_Area.get()
     x_position = entry_x.get()
     y_position = entry_y.get()
-    nom = entry_id.get()
+    RP_ID = entry_id.get()
     iface = md.find_wifi_interface()
     location = [x_position, y_position]
-    md.store_fingerprint(conn, cur, iface, location, nom)
+    
+    #Store RP data into the table representing the area
+    md.store_fingerprint(conn, cur, iface, area_name, location, RP_ID)
+
     # Print table content after each update
-    md.print_table_content(conn, cur, "fingerprints")
-    
-    
+    md.print_table_content(cur, area_name)
 
-def compute_position(Cursor,rssi_vect:list):
-    position = md.kNN_3NN(Cursor, rssi_vect) 
-    # Show the computed position to the user
-    messagebox.showinfo("Computed Position", f"Your position is: {position}")
+#Delete the fingerprints within a specific table (to update RPs)
+def Delete_table_content():
+    table_name = entry_Area.get()
+    try: 
+        cur.execute(f"DELETE FROM {table_name};")
+        conn.commit()
+        print(f"All records in the '{table_name}' table have been deleted.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")    
 
+
+def compute_position():
+    
+    #Get area name   
+    current_Area = entry_Am_at.get()
+    
+    #Get Position Tag 
+    tag = entry_Position.get()
+    
+    #Current RSSI Vector
+    current_rssi_vect = md.store_fingerprint_rssivector(tag)
+    
+    try:
+        table_list = []
+        # Get a list of table names
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        table_names = cur.fetchall()
+        for table in table_names:
+            table_list.append(table[0])
+
+        if current_Area in table_list:
+            # Create a pop-up message indicating the Computed Position as an info
+            position = md.kNN_3NN(cur, current_Area, current_rssi_vect)
+            info_text = f"\n\nYour exact current position is:\n\nX: {position[0]}, Y: {position[1]}"
+            messagebox.showinfo(f"{tag} Computed Position", info_text)
+        else:
+            # Create a pop-up message indicating the issues as a warning
+            warning_text = f"\n\nUnknown Area.\nPlease provide reference points for {current_Area} Area!"
+            messagebox.showwarning(f"Unable to pinpoint your exact location due to the following error", warning_text)
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    
 
 def cancel():
     status_label.config(text="Data collection canceled")
@@ -45,63 +129,118 @@ def cancel():
     conn.close()
     # Print to console APP termination
     print("Fingerprint Data collection or/and Posiotioning canceled") 
-    root.destroy()  # Close the GUI window
+    root.destroy()  
+# Close the GUI window
 
 
 
 
+## Admin configuration ##
+
+attempts=0
+while attempts<=3:
+    print("If you are willing to use the application as an administrator, please provide the login and password (if not, simply press Enter).\n")
+    # Prompt the user for login and password
+    entered_login = input("Enter login: ")
+    entered_password = input("Enter password: ")
+
+    
+    if entered_login == '':
+        print("You can still use the application's features through its graphical interface, as described above.") 
+        break  # Exit the loop if Enter is pressed
+    
+    # Check if the entered login and password match the predefined values
+    if entered_login == correct_login and entered_password == correct_password:
+        print("Access granted. You are now in the admin section.")
+        print("\n\n/************Admin section***********/\n\n")
+        admin_config() 
+    else:
+        attempts+=1
+        print("Access denied. Incorrect login or password.\n")
+        print(f"You still have {3-attempts} attempts\n")
+            
+
+## GUI ##
 # Create the main window
 root = tk.Tk()
-root.title("Wi-Fi Fingerprinting Data Collection")
+root.title("Positioning using Wi-Fi Fingerprint ")
 
 # Set the initial window size
-root.geometry("400x580")  # Width x Height
+root.geometry("500x680")  # Width x Height
 
-# Label at the top
-top_label = tk.Label(root, text="Enter Parameters", font=("Helvetica", 16))
-top_label.pack(pady=20, padx=10)  # Add padding and x-padding for spacing
+# Create a frame for Reference Point Collection
+First_frame = tk.Frame(root)
+First_frame.pack(padx=20, pady=40)
 
-# Create a frame for data entry
-data_frame = tk.Frame(root)
-data_frame.pack(padx=20, pady=40)
+# Create a frame for Position detection
+Second_frame = tk.Frame(root)
+Second_frame.pack(padx=20, pady=40)  # Adjust the padding as needed
 
-# Label and entry for x position
-x_label = tk.Label(data_frame, text="X:", font=("Helvetica", 12), fg="blue")
-x_label.grid(row=2, column=0, padx=10, pady=5)
-entry_x = tk.Entry(data_frame, font=("Helvetica", 12))
-entry_x.grid(row=2, column=1, padx=10, pady=5)
+# Label For the First frame 
+top_label = tk.Label(First_frame, text="Define Indoor Environnement", font=("Helvetica", 16))
+top_label.grid(row=0, column=0, pady=10, padx=10)  # Add padding and x-padding for spacing
 
-# Label and entry for y position
-y_label = tk.Label(data_frame, text="Y:", font=("Helvetica", 12), fg="blue")
-y_label.grid(row=3, column=0, padx=10, pady=5)
-entry_y = tk.Entry(data_frame, font=("Helvetica", 12))
-entry_y.grid(row=3, column=1, padx=10, pady=5)
+
+# Label For the second frame
+top_label = tk.Label(Second_frame, text="Get current position", font=("Helvetica", 16))
+top_label.grid(row=0, column=0, pady=10, padx=10)  # Add padding and x-padding for spacing
+
+# Label and entry for Area Name
+Area_label = tk.Label(First_frame, text="Area Name:", font=("Helvetica", 12), fg="blue")
+Area_label.grid(row=1, column=0, padx=10, pady=5)
+entry_Area = tk.Entry(First_frame, font=("Helvetica", 12))
+entry_Area.grid(row=1, column=1, padx=10, pady=5)
 
 # Label and entry for ID
-id_label = tk.Label(data_frame, text="ID:", font=("Helvetica", 12), fg="blue")
-id_label.grid(row=1, column=0, padx=10, pady=5)
-entry_id = tk.Entry(data_frame, font=("Helvetica", 12))
-entry_id.grid(row=1, column=1, padx=10, pady=5)
+id_label = tk.Label(First_frame, text="Reference Point ID:", font=("Helvetica", 12), fg="blue")
+id_label.grid(row=2, column=0, padx=10, pady=5)
+entry_id = tk.Entry(First_frame, font=("Helvetica", 12))
+entry_id.grid(row=2, column=1, padx=10, pady=5)
+
+# Label and entry for x position
+x_label = tk.Label(First_frame, text="Position - X:", font=("Helvetica", 12), fg="blue")
+x_label.grid(row=3, column=0, padx=10, pady=5)
+entry_x = tk.Entry(First_frame, font=("Helvetica", 12))
+entry_x.grid(row=3, column=1, padx=10, pady=5)
+
+# Label and entry for y position
+y_label = tk.Label(First_frame, text="Position - Y:", font=("Helvetica", 12), fg="blue")
+y_label.grid(row=4, column=0, padx=10, pady=5)
+entry_y = tk.Entry(First_frame, font=("Helvetica", 12))
+entry_y.grid(row=4, column=1, padx=10, pady=5)
 
 # Button to launch the scan and store data
-scan_button = tk.Button(data_frame, text="Launch Scan", command=scan, font=("Helvetica", 14), bg="green", fg="white")
-scan_button.grid(row=4, padx=15, pady=10)
+scan_button = tk.Button(First_frame, text="Record Reference Point", command=scan, font=("Helvetica", 14), bg="green", fg="white")
+scan_button.grid(row=5, padx=15, pady=10)
+
+# Button to Re-initilize the table representing the area
+scan_button = tk.Button(First_frame, text="Initialize Area", command=Delete_table_content, font=("Helvetica", 14), bg="gray", fg="white")
+scan_button.grid(row=6, padx=15, pady=10)
+
+
+# Label and entry for Position Tag
+Position_label = tk.Label(Second_frame, text="Position Tag:", font=("Helvetica", 12), fg="blue")
+Position_label.grid(row=1, column=0, padx=10, pady=5)
+entry_Position = tk.Entry(Second_frame, font=("Helvetica", 12))
+entry_Position.grid(row=1, column=1, padx=10, pady=5)
+
+
+# Label and entry for Area Name
+Am_at_label = tk.Label(Second_frame, text="Am at:", font=("Helvetica", 12), fg="blue")
+Am_at_label.grid(row=2, column=0, padx=10, pady=5)
+entry_Am_at = tk.Entry(Second_frame, font=("Helvetica", 12))
+entry_Am_at.grid(row=2, column=1, padx=10, pady=5)
 
 # Button to compute position
-compute_position_button = tk.Button(data_frame, text="Compute Position", command= lambda : compute_position(cur,Rssi_vector), font=("Helvetica", 14), bg="blue", fg="white")
-compute_position_button.grid(row=6, padx=15, pady=10)
-
-# Button to  Search for the current position --- collect current position RSSI's
-Current_Position_button = tk.Button(data_frame, text="Where AM I ?", command= lambda : md.store_fingerprint_rssivector(Rssi_vector), font=("Helvetica", 14), bg="gray", fg="white")
-Current_Position_button.grid(row=5, padx=15, pady=10)
-
-# Button to discover another area 
-New_Area_button = tk.Button(data_frame, text="New Area", command=lambda : md.delete_data(conn,cur), font=("Helvetica", 14), bg="orange", fg="white")
-New_Area_button.grid(row=7, padx=15, pady=10)
+compute_position_button = tk.Button(Second_frame, text="Where Exactly ?", command= lambda : compute_position(), font=("Helvetica", 14), bg="blue", fg="white")
+# or: compute_position_button = tk.Button(Second_frame, text="Where Exactly ?", command=compute_position, font=("Helvetica", 14), bg="blue", fg="white")
+# in case you want to pass parameters to the handler function it's necessary to use lambda.
+compute_position_button.grid(row=3, padx=15, pady=10)
 
 # Button to cancel the operation
 cancel_button = tk.Button(root, text="Cancel", command=cancel, font=("Helvetica", 14), bg="red", fg="white")
 cancel_button.pack(pady=20)
+
 
 # Label to display status
 status_label = tk.Label(root, text="", font=("Helvetica", 12))
--- Finger_Print_Technique/LAB - ENSI_Couloir_salle TP2_TP1/Modules.py
+++ Finger_Print_Technique/LAB - ENSI_Couloir_salle TP2_TP1/Modules.py
@@ -7,9 +7,12 @@ import time
 import sqlite3
 import math
 
-# Custom exception for when the interface is not found
-class InterfaceNotFoundError(Exception):
-    pass
+
+# Function to initialize the database connection
+def initialize_database():
+    conn = sqlite3.connect('wifi_fingerprints.db')
+    cursor = conn.cursor()
+    return conn, cursor
 
 # Find wifi interface 
 def find_wifi_interface():
@@ -24,10 +27,9 @@ def find_wifi_interface():
         names[i]=interface
     return names[0]
 
-find_wifi_interface()
 
 #Print the content of the table to monitor updates 
-def print_table_content(conn, cursor, table_name):
+def print_table_content(cursor, table_name):
     try:
         cursor.execute(f"SELECT * FROM {table_name}")
         rows = cursor.fetchall()
@@ -41,48 +43,24 @@ def print_table_content(conn, cursor, table_name):
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
-#Delete the fingerprint table (to make it empty in order to store another spot measurement for example: cours,un autre couloire)
-def delete_data(conn, cursor):
+#Remove the table labeled "table_name." This functionality is exclusively accessible to the administrator
+#Note:delete_table feature is not included in the GUI. 
+def delete_table(con,cur,table_name):
     try:
-        # Get a list of table names
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        table_names = cursor.fetchall()
-        
-        # Iterate through the table names and delete each table
-        for table in table_names:
-            table_name = table[0]
-            cursor.execute(f"DELETE FROM {table_name};")
-            conn.commit()
-            print(f"All records in the '{table_name}' table have been deleted.")
-    except Exception as e:
+        cur.execute(f"DROP TABLE IF EXISTS {table_name}")
+        con.commit()
+        print(f"The '{table_name}' table has been deleted.")
+    except sqlite3.Error as e:
         print(f"An error occurred: {str(e)}")
 
 
-# Initialize Wi-Fi interface
-def initialize_wifi_interface(interface_name):
-    
-    wifi = pywifi.PyWiFi()
-    # Initialize the selected Wi-Fi interface
-    iface = None
-    
-    #debug
-    print (interface_name,'\n')
-    for interface in wifi.interfaces():
-        print (interface.name(), '\n')
-    
-    for interface in wifi.interfaces():
-        if interface_name == interface.name():
-            iface = interface
-            break
-    if iface is None:
-        raise InterfaceNotFoundError(f"Interface '{interface_name}' not found. Make sure the interface exists.")
-
-# Function to scan and store fingerprints into database table
-def store_fingerprint(conn,cur,iface ,location,nom):
+# Function to scan and store fingerprints into database tables
+def store_fingerprint(conn,cur,iface,Area_name,RP_location,RP_ID):
     iface.scan()
     time.sleep(2)  # Wait for a moment to allow scanning to complete
     RSSI_values = []
     scan_results = iface.scan_results()
+
     for network in scan_results:
         bssid = network.bssid
         ssid = network.ssid
@@ -90,13 +68,19 @@ def store_fingerprint(conn,cur,iface ,location,nom):
         RSSI_values.append(signal_strength)
     # Convert the RSSI_values list to a string
     rssi_values_str = ', '.join(map(str, RSSI_values))
+
+    # Create a table specific to the Area to store Wi-Fi fingerprints (if it doesn't exist)
+    cur.execute(f'''CREATE TABLE IF NOT EXISTS {Area_name}
+                  (location_x INTEGER ,location_y INTEGER , bssid TEXT, ssid TEXT, signal_strength TEXT, nom TEXT)''')
+    conn.commit()
     
-    cur.execute('''INSERT INTO fingerprints (location_x, location_y, bssid, ssid, signal_strength, nom) 
-                      VALUES (?, ?, ?, ?, ?, ?)''', (location[0], location[1], bssid, ssid, rssi_values_str, nom))
+    cur.execute(f'''INSERT INTO {Area_name} (location_x, location_y, bssid, ssid, signal_strength, nom) 
+                      VALUES (?, ?, ?, ?, ?, ?)''', (RP_location[0], RP_location[1], bssid, ssid, rssi_values_str, RP_ID))
     conn.commit()
 
 # Function to scan and store fingerprints values for the current position  and loaded into an RSSI_vector 
-def store_fingerprint_rssivector(rssi_vector:list):
+def store_fingerprint_rssivector(Tag_name):
+    rssi_vector = [] 
     iface = find_wifi_interface()
     iface.scan()
     time.sleep(2)  # Wait for a moment to allow scanning to complete
@@ -104,32 +88,24 @@ def store_fingerprint_rssivector(rssi_vector:list):
     for network in scan_results:
         signal_strength = network.signal
         rssi_vector.append(signal_strength)
-    print(rssi_vector)
-
-
-
-# Function to initialize the database connection
-def initialize_database():
-    conn = sqlite3.connect('wifi_fingerprints.db')
-    cursor = conn.cursor()
-    # Create a table to store Wi-Fi fingerprints (if it doesn't exist)
-    cursor.execute('''CREATE TABLE IF NOT EXISTS fingerprints
-                  (location_x INTEGER ,location_y INTEGER , bssid TEXT, ssid TEXT, signal_strength TEXT, nom TEXT)''')
-    conn.commit()
-    return conn, cursor
+    print(f"Your Current Position Finger print:\n {Tag_name} : {rssi_vector}")
+    return rssi_vector
 
 
 # Compute the RSSI <distance> between two points (the current location and a reference point)
-# For two points if the two RSSI vector measurments mathces the function return 0.
+
 def euclidean_distance(vec1, vec2):
     if len(vec1) != len(vec2):
         raise ValueError("Vector dimensions do not match")
     return math.sqrt(sum((x - y) ** 2 for x, y in zip(vec1, vec2))) #zip(vec1, vec2) = ((x1,y1),(x2,y2),...)
+# Note: For two points if the two RSSI vector measurments mathces the function return 0.
 
 
-def kNN_3NN(cursor, mu_rssi_vector):
+# KNN_3NN algorithm
+def kNN_3NN(cursor, table_name, mu_rssi_vector):
     distances = {} #Store position and distance pairs as following {(Px,Py):distance}
-    cursor.execute("SELECT * FROM fingerprints")
+    cursor.execute(f"SELECT * FROM {table_name}")
+    
     for fingerprint in cursor.fetchall():
         rssi_string = fingerprint[4]  # fingerprint[4] contains the RSSI values as a string
         rssi_values = [int(value.strip()) for value in rssi_string.split(',')] # Convert the given string into a list of integers
